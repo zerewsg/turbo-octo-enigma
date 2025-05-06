4963d8d51cd01e799222876a358f3814c815c69c
zerewsg <ger62076309kyc@gmail.com>
2025-05-06 17:23:22 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+// const student = {
+//   name: "Иван",
+//   age: 20,
+//   university: "ИТМО"
+// };
+
+// const studentJSON = JSON.stringify(student);
+// const newStudent = JSON.parse(studentJSON);
+
+// console.log(newStudent);
+
+
+
+
+
+// const book = {
+//   title: "JavaScript: The Good Parts",
+//   author: "Douglas Crockford",
+//   year: 2008
+// };
+
+// const bookJSON = JSON.stringify(book);
+// const parsedBook = JSON.parse(bookJSON);
+
+// console.log(parsedBook); 
+
+
+
+
+
+// const employees = [
+//   { name: "Алексей", position: "Разработчик" },
+//   { name: "Мария", position: "Дизайнер" },
+//   { name: "Петр", position: "Менеджер" }
+// ];
+
+// const employeesJSON = JSON.stringify(employees);
+// const newEmployees = JSON.parse(employeesJSON);
+
+// console.log(newEmployees);
